33d030ed7e547f047c1199de5124f5cdfd670b5b
Christian8397 <bachci@mail.uc.edu>
2025-09-20 17:02:26 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int intOperandA = 5;
+        int intOperandB = 10;
+
+        int intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+
+
+        double doubleOperandA = 5.5;
+        double doubleOperandB = 10.5;
+        double doublesum = doubleOperandA + doubleOperandB;
+        System.out.println("the sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doublesum);
+
+        double mylunchcost = 12.50;
+        System.out.println("The cost of my lunch is " + mylunchcost);
+
+        // code for variabls and commit and push
+
+    }
+}
